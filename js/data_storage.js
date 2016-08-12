d6dde070a626e3291d30b1e53080aae54e0367be
TheAngularity <TheAngularity@users.noreply.github.com>
2016-08-12 20:52:07 +0200

    delete releaseDate entry ... not correctly maintained

--- js/data_storage.js
+++ js/data_storage.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var CONFIGURATOR = {
-    'releaseDate': 1463055533515, // new Date().getTime() - Thu May 12 2016 14:19:06 GMT+0200
-    
      // all versions are specified and compared using semantic versioning http://semver.org/
     'apiVersionAccepted': '1.2.1',
     'backupRestoreMinApiVersionAccepted': '1.5.0',
