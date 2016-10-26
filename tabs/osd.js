e2d40d78304b8c4d18d2a0ed9f6e474129411b32
nathan <nathan@vertile.com>
2016-10-25 23:35:01 -0600

    fc now sends an empty byte in response to osd msp cmds

--- tabs/osd.js
+++ tabs/osd.js
@@ -595,7 +595,8 @@ TABS.osd.initialize = function (callback) {
           MSP.promise(MSPCodes.MSP_OSD_CONFIG)
           .then(function(info) {
             OSD.chooseFields();
-            if (!info.length) {
+            // fc responsed with only 1 field: osd unsupported
+            if (info.length == 1) {
               $('.unsupported').fadeIn();
               return;
             }
