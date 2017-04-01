43f146845186856c1eb2e387e8f09b07a9524ee9
Hydra <dominic.clifton@cleanflight.com>
2017-04-01 21:59:44 +0100

    CF/BF - Add VTX to features list.

--- js/Features.js
+++ js/Features.js
@@ -72,7 +72,8 @@ var Features = function (config) {
 
         if (semver.gte(CONFIG.apiVersion, "1.20.0")) {
             features.push(
-                {bit: 18, group: 'other', name: 'OSD'}
+                {bit: 18, group: 'other', name: 'OSD'},
+                {bit: 24, group: 'other', name: 'VTX'}
             );
         }
 
