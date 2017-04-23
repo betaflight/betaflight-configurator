457700f052ee8b152e230a5f49565d8ebc7ecb39
Sean Kelly <theseankelly@outlook.com>
2017-04-23 13:06:24 -0700

    Changing the wrap around the TARGET_CUSTOM SerialRx protocol to be based on MSP Version and not FC version

--- tabs/configuration.js
+++ tabs/configuration.js
@@ -478,7 +478,7 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
             serialRXtypes.push('TARGET_CUSTOM');
         }
 
-        if (semver.gte(CONFIG.flightControllerVersion, "3.2.0"))  {
+        if (semver.gte(CONFIG.apiVersion, "1.35.0"))  {
             serialRXtypes.push('TARGET_CUSTOM');
         }
 
