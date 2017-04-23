6119acf617d8f00dc878163882445c64960b6ca5
Sean Kelly <theseankelly@outlook.com>
2017-04-23 07:53:37 -0700

    BF/CF - Adding TARGET_CUSTOM SerialRx protocol option. (#484)
    Allows configurator to choose to use custom target-defined serialRx
    protocols. Requires target support

--- tabs/configuration.js
+++ tabs/configuration.js
@@ -478,6 +478,10 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
             serialRXtypes.push('TARGET_CUSTOM');
         }
 
+        if (semver.gte(CONFIG.flightControllerVersion, "3.2.0"))  {
+            serialRXtypes.push('TARGET_CUSTOM');
+        }
+
         var serialRX_e = $('select.serialRX');
         for (var i = 0; i < serialRXtypes.length; i++) {
             serialRX_e.append('<option value="' + i + '">' + serialRXtypes[i] + '</option>');
