04e1c9d201810b48e26d53e50acd31f92fb8c821
Anders G <anders.gallon@animechtechnologies.com>
2017-05-09 17:37:25 +0200

    Add missing ')'-characters to make syntax valid

--- js/localization.js
+++ js/localization.js
@@ -9,28 +9,28 @@ function localize() {
         return chrome.i18n.getMessage(messageID);
     };
 
-    $('[i18n]:not(.i18n-replaced').each(function() {
+    $('[i18n]:not(.i18n-replaced)').each(function() {
         var element = $(this);
 
         element.html(translate(element.attr('i18n')));
         element.addClass('i18n-replaced');
     });
 
-    $('[i18n_title]:not(.i18n_title-replaced').each(function() {
+    $('[i18n_title]:not(.i18n_title-replaced)').each(function() {
         var element = $(this);
 
         element.attr('title', translate(element.attr('i18n_title')));
         element.addClass('i18n_title-replaced');
     });
 
-    $('[i18n_value]:not(.i18n_value-replaced').each(function() {
+    $('[i18n_value]:not(.i18n_value-replaced)').each(function() {
         var element = $(this);
 
         element.val(translate(element.attr('i18n_value')));
         element.addClass('i18n_value-replaced');
     });
 
-    $('[i18n_placeholder]:not(.i18n_placeholder-replaced').each(function() {
+    $('[i18n_placeholder]:not(.i18n_placeholder-replaced)').each(function() {
         var element = $(this);
 
         element.attr('placeholder', translate(element.attr('i18n_placeholder')));
