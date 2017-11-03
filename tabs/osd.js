d10a1613492a4d71e5a419cdccaad3c3608640c7
Harpalyke <harpalyke3@gmail.com>
2017-11-03 22:00:09 +0000

    Check if length of OSD.data.warnings matches that of the constants. OSD.data.warnings is empty when using a 3.2.2 firmware.

--- tabs/osd.js
+++ tabs/osd.js
@@ -928,9 +928,11 @@ OSD.msp = {
       result.push16(OSD.data.alarms.alt.value);
       if (semver.gte(CONFIG.apiVersion, "1.36.0")) {
         var warningFlags = 0;
-        for (var i = 0; i < OSD.constants.WARNINGS.length; i++) {
-          if (OSD.data.warnings[i].enabled) {
-            warningFlags |= (1 << i);
+        if(OSD.constants.WARNINGS.length === OSD.data.warnings.length){
+          for (var i = 0; i < OSD.constants.WARNINGS.length; i++) {
+            if (OSD.data.warnings[i].enabled) {
+              warningFlags |= (1 << i);
+            }
           }
         }
         console.log(warningFlags);
