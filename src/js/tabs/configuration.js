406932ce489e33dbeb4f71daa4107850533368ff
jerryxin123 <48876020+jerryxin123@users.noreply.github.com>
2019-04-04 19:37:19 +0800

    Add rx SerialType for DJI_HDL receiver

--- src/js/tabs/configuration.js
+++ src/js/tabs/configuration.js
@@ -642,6 +642,10 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
             serialRXtypes.push('FrSky FPort');
         }
 
+        if (semver.gte(CONFIG.apiVersion, "1.41.0"))  {
+            serialRXtypes.push('DJI_HDL_7MS');
+        }
+
         var serialRX_e = $('select.serialRX');
         for (var i = 0; i < serialRXtypes.length; i++) {
             serialRX_e.append('<option value="' + i + '">' + serialRXtypes[i] + '</option>');
