9bfea852eb3b7e601eeafe466a84819549d112c2
mikeller <github@ike.ch>
2019-11-08 07:12:37 +1300

    Fixed warning in tests.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,12 +5,13 @@ module.exports = function(config) {
         frameworks: ['mocha', 'chai', 'sinon-chai'],
         files: [
             './node_modules/jquery/dist/jquery.min.js',
+            './node_modules/jquery-textcomplete/dist/jquery.textcomplete.min.js',
             './node_modules/bluebird/js/browser/bluebird.min.js',
+            './node_modules/jbox/dist/jBox.min.js',
             './src/js/serial.js',
             './src/js/data_storage.js',
             './src/js/localization.js',
             './src/js/gui.js',
-            './node_modules/jquery-textcomplete/dist/jquery.textcomplete.min.js',
             './src/js/CliAutoComplete.js',
             './src/js/tabs/cli.js',
             './test/**/*.js'
