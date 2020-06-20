4c2516872319ffe4b6a6919bdb9e954b1d9a2aff
TheIsotopes <theisotopes@byom.de>
2020-06-20 17:15:58 +0200

    bump nw.js to version 0.46.2

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ const LINUX_INSTALL_DIR = '/opt/betaflight';
 var gitChangeSetId;
 
 var nwBuilderOptions = {
-    version: '0.46.0',
+    version: '0.46.2',
     files: './dist/**/*',
     macIcns: './src/images/bf_icon.icns',
     macPlist: { 'CFBundleDisplayName': 'Betaflight Configurator'},
