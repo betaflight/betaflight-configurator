b5185092c9c265fff433f8226eb2a2ea257441e7
Asizon <43983086+Asizon@users.noreply.github.com>
2020-11-04 09:31:11 +0100

    Hide hardwareId when no version detected

--- src/components/betaflight-logo/BetaflightLogo.vue
+++ src/components/betaflight-logo/BetaflightLogo.vue
@@ -79,7 +79,7 @@
           {{ firmwareId }}
         </span>
         <br />
-        <span v-if="hardwareId">
+        <span v-if="firmwareVersion && hardwareId">
           {{ $t("versionLabelTarget") }}: {{ hardwareId }}
         </span>
       </span>
