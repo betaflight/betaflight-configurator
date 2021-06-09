73bf57bb15f6548745206dbe42078b35fd4ba6af
Michael Keller <github@ike.ch>
2021-06-10 02:02:36 +1200

    Updated NW.js to 0.54.0.

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ const NODE_ENV = process.env.NODE_ENV || 'production';
 let gitChangeSetId;
 
 const nwBuilderOptions = {
-    version: '0.50.2',
+    version: '0.54.0',
     files: `${DIST_DIR}**/*`,
     macIcns: './src/images/bf_icon.icns',
     macPlist: { 'CFBundleDisplayName': 'Betaflight Configurator'},
