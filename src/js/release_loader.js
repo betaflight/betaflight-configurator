50668286fa26d1ed05c926106e92a0e87f6ff3c4
blckmn <blackman@xtra.com.au>
2022-10-19 19:26:39 +1100

    Updated to using class (rather than prototype).

--- src/js/release_loader.js
+++ src/js/release_loader.js
@@ -1,120 +1,123 @@
 'use strict';
 
-const ReleaseLoader = function (url) {
-    this._url = url;
-    this._cacheExpirationPeriod = 3600 * 1000;
-};
+class ReleaseLoader {
 
-ReleaseLoader.prototype.load = function (url, onSuccess, onFailure) {
+    constructor (url) {
+        this._url = url;
+        this._cacheExpirationPeriod = 3600 * 1000;
+    }
 
-    const dataTag = `${url}_Data`;
-    const cacheLastUpdateTag = `${url}_LastUpdate`;
+    load(url, onSuccess, onFailure) {
 
-    const result = SessionStorage.get([cacheLastUpdateTag, dataTag]);
-    const dataTimestamp = $.now();
-    const cachedData = result[dataTag];
-    const cachedLastUpdate = result[cacheLastUpdateTag];
+        const dataTag = `${url}_Data`;
+        const cacheLastUpdateTag = `${url}_LastUpdate`;
 
-    const cachedCallback = () => {
-        if (cachedData) {
-            GUI.log(i18n.getMessage('buildServerUsingCached', [url]));
+        const result = SessionStorage.get([cacheLastUpdateTag, dataTag]);
+        const dataTimestamp = $.now();
+        const cachedData = result[dataTag];
+        const cachedLastUpdate = result[cacheLastUpdateTag];
+
+        const cachedCallback = () => {
+            if (cachedData) {
+                GUI.log(i18n.getMessage('buildServerUsingCached', [url]));
+            }
+
+            onSuccess(cachedData);
+        };
+
+        if (!cachedData || !cachedLastUpdate || dataTimestamp - cachedLastUpdate > this._cacheExpirationPeriod) {
+            $.get(url, function (info) {
+                GUI.log(i18n.getMessage('buildServerLoaded', [url]));
+
+                // cache loaded info
+                const object = {};
+                object[dataTag] = info;
+                object[cacheLastUpdateTag] = $.now();
+                SessionStorage.set(object);
+                onSuccess(info);
+            }).fail(xhr => {
+                GUI.log(i18n.getMessage('buildServerLoadFailed', [url, `HTTP ${xhr.status}`]));
+                if (onFailure !== 'undefined') {
+                    onFailure();
+                } else {
+                    cachedCallback();
+                }
+            });
+        } else {
+            cachedCallback();
         }
+    }
 
-        onSuccess(cachedData);
-    };
+    loadTargets(callback) {
 
-    if (!cachedData || !cachedLastUpdate || dataTimestamp - cachedLastUpdate > this._cacheExpirationPeriod) {
-        $.get(url, function (info) {
-            GUI.log(i18n.getMessage('buildServerLoaded', [url]));
+        const url = `${this._url}/api/targets`;
+        this.load(url, callback);
+    }
 
-            // cache loaded info
-            const object = {};
-            object[dataTag] = info;
-            object[cacheLastUpdateTag] = $.now();
-            SessionStorage.set(object);
-            onSuccess(info);
+    loadTargetReleases(target, callback) {
+
+        const url = `${this._url}/api/targets/${target}`;
+        this.load(url, callback);
+    }
+
+    loadTarget(target, release, onSuccess, onFailure) {
+
+        const url = `${this._url}/api/builds/${release}/${target}`;
+        this.load(url, onSuccess, onFailure);
+    }
+
+    loadTargetHex(path, onSuccess, onFailure) {
+
+        const url = `${this._url}${path}`;
+        $.get(url, function (data) {
+            GUI.log(i18n.getMessage('buildServerLoaded', [path]));
+            onSuccess(data);
         }).fail(xhr => {
-            GUI.log(i18n.getMessage('buildServerLoadFailed', [url, `HTTP ${xhr.status}`]));
-            if (onFailure !== 'undefined') {
+            GUI.log(i18n.getMessage('buildServerLoadFailed', [path, `HTTP ${xhr.status}`]));
+            if (typeof(onFailure) !== 'undefined') {
                 onFailure();
-            } else {
-                cachedCallback();
             }
         });
-    } else {
-        cachedCallback();
     }
-};
 
-ReleaseLoader.prototype.loadTargets = function (callback) {
+    requestBuild(request, onSuccess, onFailure) {
 
-    const url = `${this._url}/api/targets`;
-    this.load(url, callback);
-};
+        const url = `${this._url}/api/builds`;
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: JSON.stringify(request),
+            contentType: "application/json",
+            dataType: "json",
+            success: function(data) {
+                data.url = `/api/builds/${data.key}/hex`;
+                onSuccess(data);
+            },
+        }).fail(xhr => {
+            GUI.log(i18n.getMessage('buildServerLoadFailed', [url, `HTTP ${xhr.status}`]));
+            if (typeof(onFailure) !== 'undefined') {
+                onFailure();
+            }
+        });
+    }
 
-ReleaseLoader.prototype.loadTargetReleases = function (target, callback) {
+    requestBuildStatus(key, onSuccess, onFailure) {
 
-    const url = `${this._url}/api/targets/${target}`;
-    this.load(url, callback);
-};
-
-ReleaseLoader.prototype.loadTarget = function (target, release, onSuccess, onFailure) {
-
-    const url = `${this._url}/api/builds/${release}/${target}`;
-    this.load(url, onSuccess, onFailure);
-};
-
-ReleaseLoader.prototype.loadTargetHex = function (path, onSuccess, onFailure) {
-
-    const url = `${this._url}${path}`;
-    $.get(url, function (data) {
-        GUI.log(i18n.getMessage('buildServerLoaded', [path]));
-        onSuccess(data);
-    }).fail(xhr => {
-        GUI.log(i18n.getMessage('buildServerLoadFailed', [path, `HTTP ${xhr.status}`]));
-        if (typeof(onFailure) !== 'undefined') {
-            onFailure();
-        }
-    });
-};
-
-ReleaseLoader.prototype.requestBuild = function (request, onSuccess, onFailure) {
-
-    const url = `${this._url}/api/builds`;
-    $.ajax({
-        url: url,
-        type: "POST",
-        data: JSON.stringify(request),
-        contentType: "application/json",
-        dataType: "json",
-        success: function(data) {
-            data.url = `/api/builds/${data.key}/hex`;
+        const url = `${this._url}/api/builds/${key}/status`;
+        $.get(url, function (data) {
+            GUI.log(i18n.getMessage('buildServerLoaded', [url]));
             onSuccess(data);
-        },
-    }).fail(xhr => {
-        GUI.log(i18n.getMessage('buildServerLoadFailed', [url, `HTTP ${xhr.status}`]));
-        if (typeof(onFailure) !== 'undefined') {
-            onFailure();
-        }
-    });
-};
+        }).fail(xhr => {
+            GUI.log(i18n.getMessage('buildServerLoadFailed', [url, `HTTP ${xhr.status}`]));
+            if (typeof(onFailure) !== 'undefined') {
+                onFailure();
+            }
+        });
+    }
 
-ReleaseLoader.prototype.requestBuildStatus = function (key, onSuccess, onFailure) {
+    loadOptions(onSuccess, onFailure) {
 
-    const url = `${this._url}/api/builds/${key}/status`;
-    $.get(url, function (data) {
-        GUI.log(i18n.getMessage('buildServerLoaded', [url]));
-        onSuccess(data);
-    }).fail(xhr => {
-        GUI.log(i18n.getMessage('buildServerLoadFailed', [url, `HTTP ${xhr.status}`]));
-        if (typeof(onFailure) !== 'undefined') {
-            onFailure();
-        }
-    });
-};
-
-ReleaseLoader.prototype.loadOptions = function (onSuccess, onFailure) {
-
-    const url = `${this._url}/api/options`;
-    this.load(url, onSuccess, onFailure);
-};
+        const url = `${this._url}/api/options`;
+        this.load(url, onSuccess, onFailure);
+    }
+}
