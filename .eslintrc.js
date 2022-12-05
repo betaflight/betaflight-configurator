6242f1e721e67d807c89a7029ae3a11f059ed870
haslinghuis <mark@numloq.nl>
2022-12-05 04:24:37 +0100

    Add support for ELRS UID (#3097)
    * Add support for ELRS UID
    
    * Add Babel
    
    * Fix review
    
    * Fix CSS

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: "module",
+        allowImportExportEverywhere: true,
     },
     extends: ["plugin:vue/recommended"],
     env: {
