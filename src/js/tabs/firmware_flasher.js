60ee559c0415377aa99fc17e45574f7ec00923fb
Míguel Ángel Mulero Martínez <mcgivergim@gmail.com>
2023-01-23 01:19:45 +0100

    Don't close on select cloud build options (#3265)

--- src/js/tabs/firmware_flasher.js
+++ src/js/tabs/firmware_flasher.js
@@ -383,7 +383,7 @@ firmware_flasher.initialize = function (callback) {
         $('select[name="radioProtocols"]').select2();
         $('select[name="telemetryProtocols"]').select2();
         $('select[name="motorProtocols"]').select2();
-        $('select[name="options"]').select2();
+        $('select[name="options"]').select2({ closeOnSelect: false });
         $('select[name="commits"]').select2({ tags: true });
 
         $('select[name="board"]').change(function() {
