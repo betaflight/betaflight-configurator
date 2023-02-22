62f5cecd71fee0d1c7da61be94d922d833a451e2
Mark Haslinghuis <mark@numloq.nl>
2023-02-22 15:35:53 +0100

    replaceAll-workaround (#3348)

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,12 @@ import { checkForConfiguratorUpdates } from './utils/checkForConfiguratorUpdates
 import * as THREE from 'three';
 import * as d3 from 'd3';
 
+if (typeof String.prototype.replaceAll === "undefined") {
+    String.prototype.replaceAll = function(match, replace) {
+        return this.replace(new RegExp(match, 'g'), () => replace);
+    };
+}
+
 $(document).ready(function () {
 
     useGlobalNodeFunctions();
