19997d6c55564c54a7db3974fbaa89128140043b
HThuren <thuren.henrik@gmail.com>
2023-04-02 17:28:35 +0200

    small changes

--- src/js/tabs/setup.js
+++ src/js/tabs/setup.js
@@ -342,7 +342,7 @@ setup.initialize = function (callback) {
             msp_api_e.text([FC.CONFIG.apiVersion]);
             build_date_e.text([FC.CONFIG.buildInfo]);
 
-            if(FC.CONFIG.buildInfo.length > 0) {
+            if (FC.CONFIG.buildKey.length > 1) {
                 const buildRoot   = `https://build.betaflight.com/api/builds/${FC.CONFIG.buildKey}`;
                 const buildConfig = `<span class="buildInfoBtn" title="${i18n.getMessage('initialSetupInfoBuildInfoConfig')}: ${buildRoot}/json">
                                      <a href="${buildRoot}/json" target="_blank"><strong>${i18n.getMessage('initialSetupInfoBuildInfoConfig')}</a></strong></span>`;
@@ -354,7 +354,7 @@ setup.initialize = function (callback) {
                 $('.build-info a').addClass('disabled');
             }
 
-            if(FC.CONFIG.buildOptions.length > 0) {
+            if (FC.CONFIG.buildOptions.length > 0) {
                 let buildOptions = "";
                 build_opt_e.text = "";
                 for (const buildOption of FC.CONFIG.buildOptions) {
