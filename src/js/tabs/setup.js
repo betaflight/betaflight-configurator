5fc5f516e8b14e87f91547cabc0a511afa5ef2a4
Mark Haslinghuis <mark@numloq.nl>
2023-11-28 14:43:20 +0100

    Add missing debugmodes and fields (#3646)
    * Add missing debugmodes and fields
    
    * Update text in setup
    
    * Fix todo

--- src/js/tabs/setup.js
+++ src/js/tabs/setup.js
@@ -364,8 +364,8 @@ setup.initialize = function (callback) {
             if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_46)) {
                 MSP.send_message(MSPCodes.MSP2_SENSOR_CONFIG_ACTIVE, false, false, function() {
                     // Sensor info
-                    const textNA = 'N/A';
-                    const textDisabled = 'disabled';
+                    const textNA = 'Not included in build';
+                    const textDisabled = 'Disabled by user';
 
                     if (FC.SENSOR_CONFIG_ACTIVE.gyro_hardware == 0xFF) {
                         sensor_gyro_e.text(textNA);
