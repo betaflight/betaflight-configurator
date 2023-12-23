772580907eccad5baa410e0d6f5718cfff160479
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2023-12-23 06:55:36 +0100

    test: add list of all the commands as describe

--- src/js/msp-cli/msp-cli.test.js
+++ src/js/msp-cli/msp-cli.test.js
@@ -1,7 +1,70 @@
 import { describe, expect, it } from "vitest";
 
+/**
+ * full command list of cli commands in firmware:
+ * https://github.com/betaflight/betaflight/blob/7d122c481a64650e99f7f31009ddf657cc983271/src/main/cli/cli.c#L6456-L6612
+ * full list of msp in firmware:
+ * https://github.com/betaflight/betaflight/blob/778088013977eb089f8545fb0223c8e355069a00/src/main/msp/msp_protocol.h
+ */
+
 describe("MSP CLI", () => {
-    it("encodes `status` command", () => {
-        expect(true).toBe(true);
-    });
+    describe.todo('adjrange');
+    describe.todo('aux');
+    describe.todo('batch');
+    describe.todo('beacon');
+    describe.todo('beeper');
+    describe.todo('bind_rx');
+    describe.todo('bl');
+    describe.todo('board_name');
+    describe.todo('color');
+    describe.todo('defaults');
+    describe.todo('diff');
+    describe.todo('dma');
+    describe.todo('dshot_telemetry_info');
+    describe.todo('dshotprog');
+    describe.todo('dump');
+    describe.todo('escprog');
+    describe.todo('exit');
+    describe.todo('feature');
+    describe.todo('flash_erase');
+    describe.todo('flash_info');
+    describe.todo('flash_read');
+    describe.todo('flash_scan');
+    describe.todo('flash_write');
+    describe.todo('get');
+    describe.todo('gpspassthrough');
+    describe.todo('gyroregisters');
+    describe.todo('help');
+    describe.todo('led');
+    describe.todo('manufacturer_id');
+    describe.todo('map');
+    describe.todo('mcu_id');
+    describe.todo('mixer');
+    describe.todo('mmix');
+    describe.todo('mode_color');
+    describe.todo('motor');
+    describe.todo('msc');
+    describe.todo('play_sound');
+    describe.todo('profile');
+    describe.todo('rateprofile');
+    describe.todo('rc_smoothing_info');
+    describe.todo('resource');
+    describe.todo('rxfail');
+    describe.todo('rxrange');
+    describe.todo('save');
+    describe.todo('sd_info');
+    describe.todo('serial');
+    describe.todo('serialpassthrough');
+    describe.todo('servo');
+    describe.todo('set');
+    describe.todo('signature');
+    describe.todo('simplified_tuning');
+    describe.todo('smix');
+    describe.todo('status');
+    describe.todo('tasks');
+    describe.todo('timer');
+    describe.todo('version');
+    describe.todo('vtx');
+    describe.todo('vtx_info');
+    describe.todo('vtxtable');
 });
