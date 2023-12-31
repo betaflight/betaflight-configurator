0765c471964e1f050770d20fbdcd32634b4c5348
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2023-12-31 19:08:56 +0100

    feat: add basic offline pwa support

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { readFileSync } from "node:fs";
 import copy from "rollup-plugin-copy";
 import pkg from './package.json';
 import * as child from 'child_process';
+import { VitePWA } from "vite-plugin-pwa";
 
 const commitHash = child.execSync('git rev-parse --short HEAD').toString();
 
@@ -72,6 +73,33 @@ export default defineConfig({
                 { src: ["locales", "resources", "src/tabs", "src/images"], dest: "src/dist" },
             ],
             hook: "writeBundle",
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json,mcm}'],
+                // 5MB
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            },
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                name: pkg.productName,
+                short_name: pkg.productName,
+                description: pkg.description,
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/images/pwa/pwa-192-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/images/pwa/pwa-512-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
         }),
     ],
     root: "./src",
