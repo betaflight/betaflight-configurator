73849107f6bd2749faffb939cfc79c0c59bf63cd
Mark Haslinghuis <mark@numloq.nl>
2024-03-21 21:28:59 +0100

    TPA Clarification (#3851)

--- src/js/tabs/pid_tuning.js
+++ src/js/tabs/pid_tuning.js
@@ -118,7 +118,7 @@ pid_tuning.initialize = function (callback) {
         if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_45)) {
             // Moved tpa to profile
             $('select[id="tpaMode"]').val(FC.ADVANCED_TUNING.tpaMode);
-            $('input[id="tpaRate"]').val(FC.ADVANCED_TUNING.tpaRate.toFixed(2));
+            $('input[id="tpaRate"]').val(FC.ADVANCED_TUNING.tpaRate * 100);
             $('input[id="tpaBreakpoint"]').val(FC.ADVANCED_TUNING.tpaBreakpoint);
         } else {
             $('.tpa-old input[name="tpa"]').val(FC.RC_TUNING.dynamic_THR_PID.toFixed(2));
@@ -1108,7 +1108,7 @@ pid_tuning.initialize = function (callback) {
 
         if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_45)) {
             FC.ADVANCED_TUNING.tpaMode = $('select[id="tpaMode"]').val();
-            FC.ADVANCED_TUNING.tpaRate = parseFloat($('input[id="tpaRate"]').val());
+            FC.ADVANCED_TUNING.tpaRate = parseInt($('input[id="tpaRate"]').val()) / 100;
             FC.ADVANCED_TUNING.tpaBreakpoint = parseInt($('input[id="tpaBreakpoint"]').val());
         } else {
             FC.RC_TUNING.dynamic_THR_PID = parseFloat($('.tpa-old input[name="tpa"]').val());
