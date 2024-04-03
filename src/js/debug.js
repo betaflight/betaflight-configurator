e03d102112f1fb1a621c751048d1364cd559e55c
Mark Haslinghuis <mark@numloq.nl>
2024-04-03 22:59:40 +0200

    Add EZLANDING debug field (#3826)
    Add debug field

--- src/js/debug.js
+++ src/js/debug.js
@@ -754,6 +754,8 @@ const DEBUG = {
             'debug[1]': 'Adjusted Throttle',
             'debug[2]': 'Upper Limit',
             'debug[3]': 'EZ Land Limit',
+            'debug[4]': 'Stick Limit',
+            'debug[5]': 'Speed Limit',
         },
     },
 
