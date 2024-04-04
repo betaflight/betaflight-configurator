11d10b207559fe41aa23bc1245fc347f66406cb8
Mark Haslinghuis <mark@numloq.nl>
2024-04-04 21:08:27 +0200

    Update version to 11.0

--- src/js/data_storage.js
+++ src/js/data_storage.js
@@ -7,12 +7,13 @@ export const API_VERSION_1_43 = '1.43.0';
 export const API_VERSION_1_44 = '1.44.0';
 export const API_VERSION_1_45 = '1.45.0';
 export const API_VERSION_1_46 = '1.46.0';
+export const API_VERSION_1_47 = '1.47.0';
 
 const CONFIGURATOR = {
     // all versions are specified and compared using semantic versioning http://semver.org/
     API_VERSION_ACCEPTED: API_VERSION_1_41,
     API_VERSION_MIN_SUPPORTED_BACKUP_RESTORE: API_VERSION_1_41,
-    API_VERSION_MAX_SUPPORTED: API_VERSION_1_46,
+    API_VERSION_MAX_SUPPORTED: API_VERSION_1_47,
 
     connectionValid: false,
     connectionValidCliOnly: false,
