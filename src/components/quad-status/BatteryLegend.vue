faf0abdf120e26ebaf6ff361d0cfde59f6e4d79e
Mark Haslinghuis <mark@numloq.nl>
2024-05-15 21:55:20 +0200

    Remove outdated comments

--- src/components/quad-status/BatteryLegend.vue
+++ src/components/quad-status/BatteryLegend.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script>
-const NO_BATTERY_VOLTAGE_MAXIMUM = 1.8; // Maybe is better to add a call to MSP_BATTERY_STATE but is not available for all versions
+const NO_BATTERY_VOLTAGE_MAXIMUM = 1.8;
 
 export default {
   props: {
