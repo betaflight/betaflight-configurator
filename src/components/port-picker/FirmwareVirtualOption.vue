63b2eb0b11fad1361a46d0ff363ed4a2b49f98fa
Mark Haslinghuis <mark@numloq.nl>
2024-06-07 15:03:34 +0200

    Remove experimental backup and restore (#4007)
    * Remove experimental backup and restore
    
    * Fixes per review

--- src/components/port-picker/FirmwareVirtualOption.vue
+++ src/components/port-picker/FirmwareVirtualOption.vue
@@ -36,9 +36,6 @@ export default {
         { value: "1.46.0", label: "MSP: 1.46 | Firmware: 4.5.*" },
         { value: "1.45.0", label: "MSP: 1.45 | Firmware: 4.4.*" },
         { value: "1.44.0", label: "MSP: 1.44 | Firmware: 4.3.*" },
-        { value: "1.43.0", label: "MSP: 1.43 | Firmware: 4.2.*" },
-        { value: "1.42.0", label: "MSP: 1.42 | Firmware: 4.1.*" },
-        { value: "1.41.0", label: "MSP: 1.41 | Firmware: 4.0.*" },
       ],
     };
   },
