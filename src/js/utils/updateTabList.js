bb7a88d83472105af6f6520e36fed09e7fb9b395
Mark Haslinghuis <mark@numloq.nl>
2024-06-22 21:20:11 +0200

    Fix servo tab not shown

--- src/js/utils/updateTabList.js
+++ src/js/utils/updateTabList.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import FC from '../fc';
 
 export function updateTabList(features) {
     const isExpertModeEnabled = $('input[name="expertModeCheckbox"]').is(':checked');
@@ -7,8 +8,8 @@ export function updateTabList(features) {
     $('#tabs ul.mode-connected li.tab_adjustments').toggle(isExpertModeEnabled);
     $('#tabs ul.mode-connected li.tab_sensors').toggle(isExpertModeEnabled);
     $('#tabs ul.mode-connected li.tab_logging').toggle(isExpertModeEnabled);
+    $('#tabs ul.mode-connected li.tab_servos').toggle(isExpertModeEnabled && FC.CONFIG?.buildOptions?.includes('USE_SERVOS'));
 
-    $('#tabs ul.mode-connected li.tab_servos').toggle(features.isEnabled('CHANNEL_FORWARDING') || features.isEnabled('SERVO_TILT'));
     $('#tabs ul.mode-connected li.tab_gps').toggle(features.isEnabled('GPS'));
     $('#tabs ul.mode-connected li.tab_led_strip').toggle(features.isEnabled('LED_STRIP'));
     $('#tabs ul.mode-connected li.tab_transponder').toggle(features.isEnabled('TRANSPONDER'));
