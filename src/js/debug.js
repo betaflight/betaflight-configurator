901f5b7693ca54cd290334a66aa1f513a2c5bc5b
Mark Haslinghuis <mark@numloq.nl>
2024-10-15 18:28:55 +0200

    Update TPA debug (Wing)

--- src/js/debug.js
+++ src/js/debug.js
@@ -813,8 +813,11 @@ function update() {
         DEBUG.fieldNames.TPA = {
             'debug[all]': 'TPA',
             'debug[0]': 'TPA Factor',
-            'debug[1]': 'TPA Pitch Angle Factor (Wing)',
-            'debug[2]': 'TPA Argument (Wing)',
+            'debug[1]': 'TPA Attitude Roll (Wing)',
+            'debug[2]': 'TPA Attitude Pitch (Wing)',
+            'debug[3]': 'TPA Throttle (Wing)',
+            'debug[4]': 'TPA Speed (Wing)',
+            'debug[5]': 'TPA Argument (Wing)',
         };
 
         DEBUG.modes.splice(DEBUG.modes.indexOf('GPS_RESCUE_THROTTLE_PID'), 1, 'AUTOPILOT_ALTITUDE');
