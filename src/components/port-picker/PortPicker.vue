c40d62642410fceb2732f221bbddcdab4eb5c50c
Mark Haslinghuis <mark@numloq.nl>
2024-11-22 23:13:30 +0100

    Update Vue

--- src/components/port-picker/PortPicker.vue
+++ src/components/port-picker/PortPicker.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
     <div class="web-port-picker">
         <PortOverrideOption
             v-if="value.selectedPort === 'manual'"
@@ -21,71 +22,103 @@
             @input="updateValue(null, $event)"
         />
     </div>
+=======
+  <div class="web-port-picker">
+    <PortOverrideOption
+      v-if="modelValue.selectedPort === 'manual'"
+      :model-value="modelValue.portOverride"
+      @update:modelValue="updateValue('portOverride', $event)"
+    />
+    <FirmwareVirtualOption
+      v-if="modelValue.selectedPort === 'virtual' && !isConnected"
+      :model-value="modelValue.virtualMspVersion"
+      @update:modelValue="updateValue('virtualMspVersion', $event)"
+    />
+    <PortsInput
+      :model-value="modelValue.selectedPort"
+      :connected-bluetooth-devices="connectedBluetoothDevices"
+      :connected-serial-devices="connectedSerialDevices"
+      :connected-usb-devices="connectedUsbDevices"
+      :disabled="disabled"
+      :show-virtual-option="showVirtualOption"
+      :show-manual-option="showManualOption"
+      @update:modelValue="updateValue(null, $event)"
+    />
+  </div>
+>>>>>>> c85044e6 (Update Vue)
 </template>
 
 <script>
-import PortOverrideOption from "./PortOverrideOption.vue";
-import FirmwareVirtualOption from "./FirmwareVirtualOption.vue";
-import PortsInput from "./PortsInput.vue";
-import CONFIGURATOR from "../../js/data_storage";
+import { defineComponent } from 'vue';
+import PortOverrideOption from './PortOverrideOption.vue';
+import FirmwareVirtualOption from './FirmwareVirtualOption.vue';
+import PortsInput from './PortsInput.vue';
+import CONFIGURATOR from '../../js/data_storage';
 
-export default {
-    components: {
-        PortOverrideOption,
-        FirmwareVirtualOption,
-        PortsInput,
+export default defineComponent({
+  components: {
+    PortOverrideOption,
+    FirmwareVirtualOption,
+    PortsInput,
+  },
+
+  props: {
+    modelValue: {
+      type: Object,
+      default: () => ({
+        selectedPort: 'noselection',
+        selectedBaud: 115200,
+        portOverride: '/dev/rfcomm0',
+        virtualMspVersion: '1.46.0',
+        autoConnect: true,
+      }),
     },
-    props: {
-        value: {
-            type: Object,
-            default: () => ({
-                selectedPort: "noselection",
-                selectedBaud: 115200,
-                portOverride: "/dev/rfcomm0",
-                virtualMspVersion: "1.46.0",
-                autoConnect: true,
-            }),
-        },
-        connectedBluetoothDevices: {
-            type: Array,
-            default: () => [],
-        },
-        connectedSerialDevices: {
-            type: Array,
-            default: () => [],
-        },
-        connectedUsbDevices: {
-            type: Array,
-            default: () => [],
-        },
-        showVirtualOption: {
-            type: Boolean,
-            default: true,
-        },
-        showManualOption: {
-            type: Boolean,
-            default: true,
-        },
-        disabled: {
-            type: Boolean,
-            default: false,
-        },
+    connectedBluetoothDevices: {
+      type: Array,
+      default: () => [],
     },
-    computed: {
-        isConnected() {
-            return CONFIGURATOR.connectionValid;
-        },
+    connectedSerialDevices: {
+      type: Array,
+      default: () => [],
     },
-    methods: {
-        updateValue(key, value) {
-            if (key != null) {
-                this.$emit("input", { ...this.value, [key]: value });
-            } else {
-                this.$emit("input", { ...this.value, ...value });
-            }
-        },
+    connectedUsbDevices: {
+      type: Array,
+      default: () => [],
     },
-};
+    showVirtualOption: {
+      type: Boolean,
+      default: true,
+    },
+    showManualOption: {
+      type: Boolean,
+      default: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  emits: [
+    'update:modelValue',
+  ],
+
+  computed: {
+    isConnected() {
+      return CONFIGURATOR.connectionValid;
+    },
+  },
+
+  methods: {
+    updateValue(key, value) {
+      if (key != null) {
+        this.$emit('update:modelValue', { ...this.modelValue, [key]: value });
+      } else {
+        this.$emit('update:modelValue', { ...this.modelValue, ...value});
+      }
+    },
+  },
+});
 </script>
 
 <style scoped>
--- src/components/status-bar/ReadingStat.vue
+++ src/components/status-bar/ReadingStat.vue
@@ -1,28 +1,33 @@
 <template>
-    <span>
-        <span class="message">{{ $t(message) }}</span>
-        <span class="value">{{ value }}</span>
-        <span v-if="unit" class="unit">
-            {{ unit }}
-        </span>
+  <span>
+    <span class="message">{{ $t(message) }}</span>
+    <span class="value">{{ modelValue }}</span>
+    <span
+      v-if="unit"
+      class="unit"
+    >
+      {{ unit }}
     </span>
+  </span>
 </template>
 
 <script>
-export default {
-    props: {
-        message: {
-            type: String,
-            default: "",
-        },
-        value: {
-            type: Number,
-            default: 0,
-        },
-        unit: {
-            type: String,
-            default: "",
-        },
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    message: {
+      type: String,
+      default: "",
     },
-};
+    modelValue: {
+      type: Number,
+      default: 0,
+    },
+    unit: {
+      type: String,
+      default: "",
+    },
+  },
+});
 </script>
