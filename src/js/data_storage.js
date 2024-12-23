7a5b6eba4bbedfa9f78cb908305212e99700528f
blckmn <blckmn@betaflight.com>
2024-12-24 01:41:05 +1100

    May not require translation

--- src/js/data_storage.js
+++ src/js/data_storage.js
@@ -3,7 +3,6 @@ export const API_VERSION_1_44 = '1.44.0';
 export const API_VERSION_1_45 = '1.45.0';
 export const API_VERSION_1_46 = '1.46.0';
 export const API_VERSION_1_47 = '1.47.0';
-export const API_VERSION_1_48 = '1.48.0';
 
 const CONFIGURATOR = {
     // all versions are specified and compared using semantic versioning http://semver.org/
