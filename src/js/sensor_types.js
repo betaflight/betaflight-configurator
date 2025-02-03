ababba268f04979de2ce31c97a589a219830c640
Mark Haslinghuis <mark@numloq.nl>
2025-02-03 20:50:25 +0100

    Fix sonar

--- src/js/sensor_types.js
+++ src/js/sensor_types.js
@@ -108,28 +108,44 @@ export function sensorTypes() {
     // remove deprecated sensors or add new ones
     if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_47)) {
         const gyroIndexL3G4200D = gyroElements.indexOf("L3G4200D");
-        if (gyroIndexL3G4200D !== -1) gyroElements.splice(gyroIndexL3G4200D, 1);
+        if (gyroIndexL3G4200D !== -1) {
+            gyroElements.splice(gyroIndexL3G4200D, 1);
+        }
 
         const gyroIndexMPU3050 = gyroElements.indexOf("MPU3050");
-        if (gyroIndexMPU3050 !== -1) gyroElements.splice(gyroIndexMPU3050, 1);
+        if (gyroIndexMPU3050 !== -1) {
+            gyroElements.splice(gyroIndexMPU3050, 1);
+        }
 
         const gyroIndexLSM6DSV16X = gyroElements.indexOf("LSM6DSV16X");
-        if (gyroIndexLSM6DSV16X !== -1) gyroElements.splice(gyroIndexLSM6DSV16X + 1, 0, "IIM42653");
+        if (gyroIndexLSM6DSV16X !== -1) {
+            gyroElements.splice(gyroIndexLSM6DSV16X + 1, 0, "IIM42653");
+        }
 
         const accIndexADXL345 = accElements.indexOf("ADXL345");
-        if (accIndexADXL345 !== -1) accElements.splice(accIndexADXL345, 1);
+        if (accIndexADXL345 !== -1) {
+            accElements.splice(accIndexADXL345, 1);
+        }
 
         const accIndexMMA8452 = accElements.indexOf("MMA8452");
-        if (accIndexMMA8452 !== -1) accElements.splice(accIndexMMA8452, 1);
+        if (accIndexMMA8452 !== -1) {
+            accElements.splice(accIndexMMA8452, 1);
+        }
 
         const accIndexBMA280 = accElements.indexOf("BMA280");
-        if (accIndexBMA280 !== -1) accElements.splice(accIndexBMA280, 1);
+        if (accIndexBMA280 !== -1) {
+            accElements.splice(accIndexBMA280, 1);
+        }
 
         const accIndexLSM303DLHC = accElements.indexOf("LSM303DLHC");
-        if (accIndexLSM303DLHC !== -1) accElements.splice(accIndexLSM303DLHC, 1);
+        if (accIndexLSM303DLHC !== -1) {
+            accElements.splice(accIndexLSM303DLHC, 1);
+        }
 
         const accIndexLSM6DSV16X = accElements.indexOf("LSM6DSV16X");
-        if (accIndexLSM6DSV16X !== -1) accElements.splice(accIndexLSM6DSV16X + 1, 0, "IIM42653");
+        if (accIndexLSM6DSV16X !== -1) {
+            accElements.splice(accIndexLSM6DSV16X + 1, 0, "IIM42653");
+        }
     }
 
     return sensorTypes;
