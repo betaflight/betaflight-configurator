a0f6b658dde2bb363d97a9374e004573f4ca2f2f
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2025-02-11 09:10:08 -0600

    fixup semver and padding
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- src/js/VirtualFC.js
+++ src/js/VirtualFC.js
@@ -4,6 +4,8 @@ import Beepers from "./Beepers";
 import FC from "./fc";
 import CONFIGURATOR from "./data_storage";
 import { OSD } from "./tabs/osd";
+import semver from "semver";
+import { API_VERSION_1_47 } from "./data_storage";
 
 const VirtualFC = {
     // these values are manufactured to unlock all the functionality of the configurator, they dont represent actual hardware
@@ -178,13 +180,11 @@ const VirtualFC = {
             "ARM",
             "ANGLE",
             "HORIZON",
-            "ALTHOLD",
             "ANTI GRAVITY",
             "MAG",
             "HEADFREE",
             "HEADADJ",
             "CAMSTAB",
-            "POS HOLD",
             "PASSTHRU",
             "BEEPERON",
             "LEDLOW",
@@ -222,13 +222,14 @@ const VirtualFC = {
             "BEEPER MUTE",
             "READY",
             "LAP TIMER RESET",
-            "CHIRP",
         ];
-    if (semver.gte(virtualFC.CONFIG.apiVersion, API_VERSION_1_47)) {
-        virtualFC.AUX_CONFIG.splice(virtualFC.AUX_CONFIG.indexOf("HORIZON") + 1, 0, "ALT_HOLD");
-        virtualFC.AUX_CONFIG.splice(virtualFC.AUX_CONFIG.indexOf("CAMSTAB") + 1, 0, "POS_HOLD");
-        virtualFC.AUX_CONFIG.push("CHIRP");
-    }
+
+        if (semver.gte(virtualFC.CONFIG.apiVersion, API_VERSION_1_47)) {
+            virtualFC.AUX_CONFIG.splice(virtualFC.AUX_CONFIG.indexOf("HORIZON") + 1, 0, "ALT_HOLD");
+            virtualFC.AUX_CONFIG.splice(virtualFC.AUX_CONFIG.indexOf("CAMSTAB") + 1, 0, "POS_HOLD");
+            virtualFC.AUX_CONFIG.push("CHIRP");
+        }
+
         FC.AUX_CONFIG_IDS = [
             0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 15, 17, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
             36, 37, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
