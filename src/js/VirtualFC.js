f053a4d0c1975e9644277e6be6dc74e4723e51ab
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2025-02-11 08:58:21 -0600

    Update src/js/VirtualFC.js -- splice for 1.47
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- src/js/VirtualFC.js
+++ src/js/VirtualFC.js
@@ -224,6 +224,11 @@ const VirtualFC = {
             "LAP TIMER RESET",
             "CHIRP",
         ];
+    if (semver.gte(virtualFC.CONFIG.apiVersion, API_VERSION_1_47)) {
+        virtualFC.AUX_CONFIG.splice(virtualFC.AUX_CONFIG.indexOf("HORIZON") + 1, 0, "ALT_HOLD");
+        virtualFC.AUX_CONFIG.splice(virtualFC.AUX_CONFIG.indexOf("CAMSTAB") + 1, 0, "POS_HOLD");
+        virtualFC.AUX_CONFIG.push("CHIRP");
+    }
         FC.AUX_CONFIG_IDS = [
             0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 15, 17, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
             36, 37, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
