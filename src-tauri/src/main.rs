88877ecb55166f87a75ac3a73c93e4b00f191747
Mark Haslinghuis <mark@numloq.nl>
2025-03-11 20:54:46 +0100

    Add initial Tauri v2 support

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
