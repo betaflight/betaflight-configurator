a2b568cf33a7c862eec6a8e1839814e0b303300d
Ramon Saraiva <ramonsaraiva@gmail.com>
2025-03-20 22:58:51 -0300

    Add tab_container reveal/hide behavior

--- src/js/main.js
+++ src/js/main.js
@@ -307,9 +307,22 @@ function startProcess() {
                 }
             });
         }
+
+        $(".tab_container").removeClass("reveal");
+        $("#background").hide();
     });
 
     $("#tabs ul.mode-disconnected li a:first").click();
+
+    $("#menu_btn").on("click", function () {
+        $(".tab_container").toggleClass("reveal");
+        $("#background").toggle();
+    });
+
+    $("#background").on("click", function () {
+        $(".tab_container").removeClass("reveal");
+        $("#background").hide();
+    });
 
     // listen to all input change events and adjust the value within limits if necessary
     $("#content").on("focus", 'input[type="number"]', function () {
