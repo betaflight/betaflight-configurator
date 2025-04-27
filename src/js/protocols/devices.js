0eef102e3ea20c636d6b91afaaf9143d0645986f
Mark Haslinghuis <mark@numloq.nl>
2025-04-27 20:58:17 +0200

    Reorder

--- src/js/protocols/devices.js
+++ src/js/protocols/devices.js
@@ -48,10 +48,10 @@ export const serialDevices = [
     { vendorId: 1155, productId: 12886 }, // STM32 in HID mode
     { vendorId: 1155, productId: 14158 }, // 0483:374e STM Electronics STLink Virtual COM Port (NUCLEO boards)
     { vendorId: 1155, productId: 22336 }, // STM Electronics Virtual COM Port
-    { vendorId: 10473, productId: 394 },  // GD32 VCP
     { vendorId: 4292, productId: 60000 }, // CP210x
     { vendorId: 4292, productId: 60001 }, // CP210x
     { vendorId: 4292, productId: 60002 }, // CP210x
+    { vendorId: 10473, productId: 394 },  // GD32 VCP
     { vendorId: 11836, productId: 22336 }, // AT32 VCP
     { vendorId: 12619, productId: 22336 }, // APM32 VCP
 ];
