f78e9b095589d87e00110dc70a28bed66ffd4ea9
LarryKarhu <advlazeris@gmail.com>
2025-05-07 19:09:31 +0600

    add switch TF-NOVA

--- src/js/sensor_types.js
+++ src/js/sensor_types.js
@@ -95,7 +95,7 @@ export function sensorTypes() {
         },
         sonar: {
             name: "Sonar",
-            elements: ["NONE", "HCSR04", "TFMINI", "TF02", "MTF01", "MTF02", "MTF01P", "MTF02P"],
+            elements: ["NONE", "HCSR04", "TFMINI", "TF02", "TFNOVA", "MTF01", "MTF02", "MTF01P", "MTF02P"],
         },
         opticalflow: {
             name: "Optical Flow",
