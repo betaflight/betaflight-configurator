61dad5cec154501d4cd8d023c6ec703d3598284a
Mark Haslinghuis <mark@numloq.nl>
2025-05-14 20:45:46 +0200

    Add custom Capacitor plugin-socket

--- android/app/src/main/java/betaflight/configurator/MainActivity.java
+++ android/app/src/main/java/betaflight/configurator/MainActivity.java
@@ -1,5 +1,6 @@
 package betaflight.configurator;
 
 import com.getcapacitor.BridgeActivity;
+import betaflight.configurator.plugin.SocketPlugin;
 
 public class MainActivity extends BridgeActivity {}
