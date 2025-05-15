affbe27453efc8d872a576bd4ff638675c9b692c
Mark Haslinghuis <mark@numloq.nl>
2025-05-15 20:56:00 +0200

    Rabbit fixes

--- android/app/src/main/java/betaflight/configurator/MainActivity.java
+++ android/app/src/main/java/betaflight/configurator/MainActivity.java
@@ -1,6 +1,13 @@
 package betaflight.configurator;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import betaflight.configurator.plugin.SocketPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(SocketPlugin.class);
+  }
+}
