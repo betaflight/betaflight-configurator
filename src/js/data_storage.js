c4f6137f90dd384ace018a667bbac17759831a7c
Mark Haslinghuis <mark@numloq.nl>
2025-10-18 01:58:46 +0200

    Add support for API 1.48 (#4668)
    * Add support for API 1.48
    
    * Refactor problems reported to allow cli
    
    * Prioritize on version modal first
    
    * Good nitpick
    
    * check version mismatch in a separate function
    
    * sonar nitpick
    
    * More nitpicks
    
    * More nitpicks II
    
    * Good catch II
    
    * Fix checkApiVersionCompatibily check
    
    * Fix checkReportProblems invocation
    
    * Refactor
    
    * Cleanup
    
    * Fix message
    
    * Fix message...
    
    * Reduce
    
    * Abstract
    
    * Meh
    
    * Reuse existing localization
    
    * Oops - revert testing

--- src/js/data_storage.js
+++ src/js/data_storage.js
@@ -3,11 +3,12 @@ export const API_VERSION_1_44 = "1.44.0";
 export const API_VERSION_1_45 = "1.45.0";
 export const API_VERSION_1_46 = "1.46.0";
 export const API_VERSION_1_47 = "1.47.0";
+export const API_VERSION_1_48 = "1.48.0";
 
 const CONFIGURATOR = {
     // all versions are specified and compared using semantic versioning http://semver.org/
     API_VERSION_ACCEPTED: API_VERSION_1_44,
-    API_VERSION_MAX_SUPPORTED: API_VERSION_1_47,
+    API_VERSION_MAX_SUPPORTED: API_VERSION_1_48,
 
     connectionValid: false,
     connectionValidCliOnly: false,
