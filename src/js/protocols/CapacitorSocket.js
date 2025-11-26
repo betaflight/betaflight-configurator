887e1b48cbd75cd0174368ceb2994864193a631e
Mark Haslinghuis <mark@numloq.nl>
2025-11-26 17:37:33 +0100

    Fix bytesSent

--- src/js/protocols/CapacitorSocket.js
+++ src/js/protocols/CapacitorSocket.js
@@ -178,7 +178,7 @@ class CapacitorSocket extends EventTarget {
         }
 
         return {
-            bytesSent: data.byteLength,
+            bytesSent: this.connected ? data.byteLength : 0,
         };
     }
 }
