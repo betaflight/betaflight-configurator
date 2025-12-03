10f552a11a2d8c7b11e655b6f467538323d711b8
Mark Haslinghuis <mark@numloq.nl>
2025-12-03 02:25:13 +0100

    Rename

--- src/js/serial.js
+++ src/js/serial.js
@@ -4,7 +4,7 @@ import Websocket from "./protocols/WebSocket.js";
 import VirtualSerial from "./protocols/VirtualSerial.js";
 import { isAndroid } from "./utils/checkCompatibility.js";
 import CapacitorSerial from "./protocols/CapacitorSerial.js";
-import CapacitorSocket from "./protocols/CapacitorSocket.js";
+import CapacitorTcp from "./protocols/CapacitorTcp.js";
 
 /**
  * Base Serial class that manages all protocol implementations
@@ -23,7 +23,7 @@ class Serial extends EventTarget {
         if (isAndroid()) {
             this._protocols = [
                 { name: "serial", instance: new CapacitorSerial() },
-                { name: "tcp", instance: new CapacitorSocket() },
+                { name: "tcp", instance: new CapacitorTcp() },
             ];
         } else {
             this._protocols = [
