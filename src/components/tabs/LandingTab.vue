69c26e0c4c257fb7b3436250d52dba04a39ea333
Eric <erics1337@gmail.com>
2025-12-05 01:43:08 -0700

    feat: Migrate Help and Landing tabs from jQuery to Vue
    This PR begins the migration of jQuery-based tabs to Vue 3 Single File
    Components as part of the ongoing modernization effort.
    
    Changes:
    - Add BaseTab.vue component for common tab lifecycle management
    - Add HelpTab.vue - static help content using Vue i18n
    - Add LandingTab.vue - landing page with reactive language switcher
    - Add vue_tab_mounter.js utility for mounting Vue tabs into #content
    - Update vue_components.js with VueTabComponents registry
    - Update main.js to use mountVueTab() for help and landing cases
    
    This is Phase 1 of a multi-phase migration approach, starting with the
    simplest static tabs to establish patterns before migrating more complex
    configuration and visualization tabs.

---
+++ src/components/tabs/LandingTab.vue
@@ -0,0 +1,131 @@
+<template>
+    <BaseTab tab-name="landing">
+        <div class="content_wrapper">
+            <div class="content_top">
+                <div class="logowrapper" align="center">
+                    <img src="./images/bf_logo_white.svg" alt="" />
+                    <div align="center">{{ $t("defaultWelcomeIntro") }}</div>
+                </div>
+            </div>
+            <div class="tab_sponsor" ref="sponsorContainer"></div>
+            <div class="content_mid grid-row">
+                <div class="column third_left text1 grid-col col4">
+                    <div class="wrap">
+                        <h2>{{ $t("defaultWelcomeHead") }}</h2>
+                        <div>{{ $t("defaultWelcomeText") }}</div>
+                    </div>
+                </div>
+                <div class="column third_center text2 grid-col col5">
+                    <div class="wrap">
+                        <h2>{{ $t("defaultContributingHead") }}</h2>
+                        <div>{{ $t("defaultContributingText") }}</div>
+                    </div>
+                </div>
+                <div class="column third_right text3 grid-col col3">
+                    <div class="wrap2">
+                        <h3>{{ $t("defaultDonateHead") }}</h3>
+                        <div>{{ $t("defaultDonateText") }}</div>
+                        <div class="donate">
+                            <a
+                                href="https://paypal.me/betaflight"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                :title="$t('defaultDonate')"
+                            >
+                                <img src="./images/btn-donate.png" alt="Paypal" height="30" />
+                            </a>
+                        </div>
+                        <div>{{ $t("defaultDonateBottom") }}</div>
+                    </div>
+                </div>
+                <div class="content_mid_bottom">
+                    <div class="socialMediaParagraph">
+                        <div class="logoSocialMedia">
+                            <img src="./images/flogo_RGB_HEX-1024.svg" alt="Facebook" class="facebookLogo" />
+                        </div>
+                        <div class="socialMediaText">{{ $t("defaultFacebookText") }}</div>
+                    </div>
+                    <div class="socialMediaParagraph">
+                        <div class="logoSocialMedia">
+                            <img src="./images/discord-logo-color.svg" alt="Discord" class="discordLogo" />
+                        </div>
+                        <div class="socialMediaText">{{ $t("defaultDiscordText") }}</div>
+                    </div>
+                </div>
+                <div class="content_bottom">
+                    <div class="statsCollection">{{ $t("statisticsDisclaimer") }}</div>
+                </div>
+                <div class="content_foot">
+                    <div class="languageSwitcher">
+                        <span>{{ $t("language_choice_message") }}</span>
+                        <a
+                            v-for="lang in availableLanguages"
+                            :key="lang"
+                            href="#"
+                            :lang="lang"
+                            :class="{ selected_language: lang === selectedLanguage }"
+                            @click.prevent="changeLanguage(lang)"
+                        >
+                            {{ $t(`language_${lang}`) }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BaseTab>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import $ from "jquery";
+import BaseTab from "./BaseTab.vue";
+import GUI from "../../js/gui";
+import { i18n } from "../../js/localization";
+import Sponsor from "../../js/Sponsor";
+
+export default defineComponent({
+    name: "LandingTab",
+    components: {
+        BaseTab,
+    },
+    setup() {
+        const sponsorContainer = ref(null);
+        const sponsor = new Sponsor();
+
+        // Get available languages including DEFAULT
+        const availableLanguages = ref(["DEFAULT", ...i18n.getLanguagesAvailables()]);
+        const selectedLanguage = ref(i18n.selectedLanguage);
+
+        function changeLanguage(lang) {
+            if (i18n.selectedLanguage !== lang) {
+                i18n.changeLanguage(lang);
+                selectedLanguage.value = lang;
+            }
+        }
+
+        onMounted(() => {
+            // Load sponsor tile - wrap with jQuery for Sponsor.js compatibility
+            if (sponsorContainer.value) {
+                sponsor.loadSponsorTile("landing", $(sponsorContainer.value));
+            }
+            GUI.content_ready();
+        });
+
+        return {
+            sponsorContainer,
+            availableLanguages,
+            selectedLanguage,
+            changeLanguage,
+        };
+    },
+});
+</script>
+
+<style scoped>
+.selected_language {
+    font-weight: bold;
+}
+.languageSwitcher a {
+    margin-left: 8px;
+}
+</style>
