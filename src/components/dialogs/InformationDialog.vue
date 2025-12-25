cbdf9b51922736f54a316dd1f0085a95c1a2faff
Eric <erics1337@gmail.com>
2025-12-25 11:56:34 -0700

    feat: Implement Information and Wait Dialogs

---
+++ src/components/dialogs/InformationDialog.vue
@@ -0,0 +1,47 @@
+<template>
+    <dialog class="dialogInformation" open>
+        <h3 class="dialogInformationTitle">{{ title }}</h3>
+        <div class="dialogInformationContent" v-html="text"></div>
+        <div class="buttons">
+            <a href="#" class="dialogInformation-confirmButton regular-button" @click.prevent="$emit('confirm')">{{
+                confirmText
+            }}</a>
+        </div>
+    </dialog>
+    <div class="dialog-backdrop"></div>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    text: String,
+    confirmText: {
+        type: String,
+        default: "OK",
+    },
+});
+
+defineEmits(["confirm"]);
+</script>
+
+<style scoped>
+.dialog-backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+}
+
+.dialogInformation {
+    display: block;
+    z-index: 1000;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+}
+</style>
---
+++ src/components/dialogs/WaitDialog.vue
@@ -0,0 +1,50 @@
+<template>
+    <dialog class="dialogWait" open>
+        <div class="data-loading"></div>
+        <h3 class="dialogWaitTitle">{{ title }}</h3>
+        <div class="buttons" v-if="showCancel">
+            <a href="#" class="dialogWait-cancelButton regular-button" @click.prevent="$emit('cancel')">{{
+                cancelText
+            }}</a>
+        </div>
+    </dialog>
+    <div class="dialog-backdrop"></div>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    showCancel: {
+        type: Boolean,
+        default: true,
+    },
+    cancelText: {
+        type: String,
+        default: "Cancel",
+    },
+});
+
+defineEmits(["cancel"]);
+</script>
+
+<style scoped>
+.dialog-backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+}
+
+.dialogWait {
+    display: block;
+    z-index: 1000;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+}
+</style>
