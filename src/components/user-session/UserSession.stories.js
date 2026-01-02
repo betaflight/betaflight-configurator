589d65458e6f5a09069926b07b1ad135cde9de2a
blckmn <blckmn@betaflight.com>
2026-01-03 07:17:03 +1100

    Removing copy paste

--- src/components/user-session/UserSession.stories.js
+++ src/components/user-session/UserSession.stories.js
@@ -1,36 +1,21 @@
 import UserSession from "./UserSession.vue";
 
+// Helper function to create story render configuration
+const createStory = () => ({
+    render: (args) => ({
+        components: { UserSession },
+        setup() {
+            return args;
+        },
+        template: "<UserSession />",
+    }),
+});
+
 export default {
     title: "Components/UserSession",
     component: UserSession,
 };
 
-export const Default = {
-    render: (args) => ({
-        components: { UserSession },
-        setup() {
-            return args;
-        },
-        template: "<UserSession />",
-    }),
-};
-
-export const LoggedOut = {
-    render: (args) => ({
-        components: { UserSession },
-        setup() {
-            return args;
-        },
-        template: "<UserSession />",
-    }),
-};
-
-export const LoggedIn = {
-    render: (args) => ({
-        components: { UserSession },
-        setup() {
-            return args;
-        },
-        template: "<UserSession />",
-    }),
-};
+export const Default = createStory();
+export const LoggedOut = createStory();
+export const LoggedIn = createStory();
