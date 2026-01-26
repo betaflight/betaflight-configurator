034e6f867f2abde5769aa17a554c60d8dbb5990a
Mark Haslinghuis <mark@numloq.nl>
2026-01-26 18:22:10 +0100

    Cleanup after using CLI over MSP instead of MSP

--- src/js/fc.js
+++ src/js/fc.js
@@ -162,7 +162,6 @@ const FC = {
     FEATURE_CONFIG: null,
     FILTER_CONFIG: null,
     GYRO_SENSOR: null,
-    SENSOR_NAMES: null,
     GPS_CONFIG: null,
     COMPASS_CONFIG: null,
     GPS_DATA: null,
@@ -609,16 +608,6 @@ const FC = {
         this.GYRO_SENSOR = {
             gyro_count: 0,
             gyro_hardware: [],
-        };
-
-        this.SENSOR_NAMES = {
-            acc: [],
-            gyro: [],
-            baro: [],
-            mag: [],
-            gps: [],
-            sonar: [],
-            opticalflow: [],
         };
 
         this.RX_CONFIG = {
