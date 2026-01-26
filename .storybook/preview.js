2c4563d405a59182edd64e6e37b744dcdb71edbf
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2026-01-26 01:41:01 +0100

    Refactor: Update CSS imports from .css to .less in multiple files for consistency

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,36 +7,36 @@ import i18nextXHRBackend from "i18next-xhr-backend";
  * Logic from ../src/js/localization.js
  */
 function parseInputFile(data) {
-  // Remove the $n interpolate of Chrome $1, $2, ... -> {{1}}, {{2}}, ...
-  const REGEXP_CHROME = /\$([1-9])/g;
-  const dataChrome = data.replace(REGEXP_CHROME, "{{$1}}");
+    // Remove the $n interpolate of Chrome $1, $2, ... -> {{1}}, {{2}}, ...
+    const REGEXP_CHROME = /\$([1-9])/g;
+    const dataChrome = data.replace(REGEXP_CHROME, "{{$1}}");
 
-  // Remove the .message of the nesting $t(xxxxx.message) -> $t(xxxxx)
-  const REGEXP_NESTING = /\$t\(([^\)]*).message\)/g;
-  const dataNesting = dataChrome.replace(REGEXP_NESTING, "$t($1)");
+    // Remove the .message of the nesting $t(xxxxx.message) -> $t(xxxxx)
+    const REGEXP_NESTING = /\$t\(([^\)]*).message\)/g;
+    const dataNesting = dataChrome.replace(REGEXP_NESTING, "$t($1)");
 
-  // Move the .message of the json object to root xxxxx.message -> xxxxx
-  const jsonData = JSON.parse(dataNesting);
-  Object.entries(jsonData).forEach(([key, value]) => {
-    jsonData[key] = value.message;
-  });
+    // Move the .message of the json object to root xxxxx.message -> xxxxx
+    const jsonData = JSON.parse(dataNesting);
+    Object.entries(jsonData).forEach(([key, value]) => {
+        jsonData[key] = value.message;
+    });
 
-  return jsonData;
+    return jsonData;
 }
 
 i18next.use(i18nextXHRBackend).init({
-  lng: "en",
-  debug: true,
-  getAsync: false,
-  ns: ["messages"],
-  defaultNS: ["messages"],
-  fallbackLng: {
-    default: ["en"],
-  },
-  backend: {
-    loadPath: "/locales/{{lng}}/{{ns}}.json",
-    parse: parseInputFile,
-  },
+    lng: "en",
+    debug: true,
+    getAsync: false,
+    ns: ["messages"],
+    defaultNS: ["messages"],
+    fallbackLng: {
+        default: ["en"],
+    },
+    backend: {
+        loadPath: "/locales/{{lng}}/{{ns}}.json",
+        parse: parseInputFile,
+    },
 });
 
 Vue.use(VueI18Next);
@@ -44,25 +44,25 @@ Vue.use(VueI18Next);
 const i18n = new VueI18Next(i18next);
 
 export const decorators = [
-  (story) => ({
-    i18n,
-    components: { story },
-    template: `
+    (story) => ({
+        i18n,
+        components: { story },
+        template: `
     <div style="margin: 1rem;">
       <link rel="stylesheet" href="/css/opensans_webfontkit/fonts.css" />
-      <link rel="stylesheet" href="/css/theme.css" />
+      <link rel="stylesheet" href="/css/theme.less" />
       <story />
     </div>
 `,
-  }),
+    }),
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 };
--- src/js/browserReceiverMsp.js
+++ src/js/browserReceiverMsp.js
@@ -2,7 +2,7 @@ import "../js/jqueryPlugins";
 import "../../libraries/jquery.nouislider.min.css";
 import "../../libraries/jquery.nouislider.pips.min.css";
 
-import "../css/theme.css";
+import "../css/theme.less";
 import "../css/main.less";
 import "../css/receiver_msp/receiver_msp.less";
 import "../css/dark-theme.less";
