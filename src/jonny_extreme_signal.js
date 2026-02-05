960ff2c8c0842e47c4ba6bf78d6ef304864c0499
Jonny Alva <shadowxdog805@gmail.com>
2026-02-05 02:54:54 -0800

    feat: add Jonny's Extreme Environment Signal Reliability preset

---
+++ src/jonny_extreme_signal.js
@@ -0,0 +1,14 @@
+/**
+ * AUTHOR: Jonny Alva
+ * PROJECT: FPV Field Operations - Extreme Environment Preset
+ * MISSION: Optimization for high-interference and low-resource logging.
+ */
+
+const extremeSignalPreset = {
+    linkQualityThreshold: 85,
+    telemetryPriority: "HIGH",
+    interferenceMitigation: "ACTIVE",
+    lowResourceLogging: true
+};
+
+export default extremeSignalPreset;
