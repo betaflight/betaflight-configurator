a2a6b62651f699dc3b68ee4d0881ac1af3559963
Eric <erics1337@gmail.com>
2026-03-10 21:12:18 -0600

    feat: migrate transponder tab to Vue 3/Pinia

--- src/js/vue_components.js
+++ src/js/vue_components.js
@@ -31,6 +31,7 @@ import ReceiverTab from "../components/tabs/ReceiverTab.vue";
 import OsdTab from "../components/tabs/OsdTab.vue";
 import SetupTab from "../components/tabs/SetupTab.vue";
 import PidTuningTab from "../components/tabs/PidTuningTab.vue";
+import TransponderTab from "../components/tabs/TransponderTab.vue";
 
 // Registry of Vue tab components - used by main.js for dynamic mounting
 export const VueTabComponents = {
@@ -58,6 +59,7 @@ export const VueTabComponents = {
     osd: OsdTab,
     setup: SetupTab,
     pid_tuning: PidTuningTab,
+    transponder: TransponderTab,
     // Move motors before pid_tuning if present in the future
 };
 
@@ -94,5 +96,6 @@ export const BetaflightComponents = {
         app.component("OsdTab", OsdTab);
         app.component("SetupTab", SetupTab);
         app.component("PidTuningTab", PidTuningTab);
+        app.component("TransponderTab", TransponderTab);
     },
 };
