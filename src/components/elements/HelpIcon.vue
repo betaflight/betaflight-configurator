b5e3ee4c3d7166812c381107fd8461bf7d833bd3
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2026-04-10 17:11:05 +0200

    Refactor: Nuxt UI - Options Tab
    - Provide custom elements for UI boxes, help icons and input rows
    - Replace existing input elements with Nuxt UI alternatives
    - Set up further Nuxt UI theme changes

---
+++ src/components/elements/HelpIcon.vue
@@ -0,0 +1,16 @@
+<template>
+    <UTooltip :text="text" :delayDuration="0" arrow>
+        <div class="p-0.5 rounded-full hover:bg-neutral-100/30 cursor-pointer duration-100 w-fit">
+            <UIcon name="i-lucide-circle-question-mark" class="size-4" />
+        </div>
+    </UTooltip>
+</template>
+
+<script setup>
+defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+});
+</script>
---
+++ src/components/elements/SettingRow.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex items-center gap-2">
+        <slot name="default"></slot>
+        <div class="flex" :class="{ 'flex-1': fullWidth }">
+            <slot name="label"></slot>
+            <span v-if="label">{{ label }}</span>
+        </div>
+        <HelpIcon v-if="help" :text="help" class="text-dimmed" />
+    </div>
+</template>
+
+<script setup>
+import HelpIcon from "./HelpIcon.vue";
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: "",
+        required: false,
+    },
+    help: {
+        type: String,
+        default: "",
+        required: false,
+    },
+    fullWidth: {
+        type: Boolean,
+        default: false,
+        required: false,
+    },
+});
+</script>
