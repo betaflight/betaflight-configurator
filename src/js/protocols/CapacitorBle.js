2a3e6c2362e6b9d9430b998664840ddc83417dad
Mark Haslinghuis <mark@numloq.nl>
2026-07-12 01:57:46 +0200

    refactor(protocols): extract shared byte/string conversion helpers into utils/bytes.js (#5264)

--- src/js/protocols/CapacitorBle.js
+++ src/js/protocols/CapacitorBle.js
@@ -1,27 +1,9 @@
 import { Capacitor } from "@capacitor/core";
 import { bluetoothDevices } from "./devices";
+import { base64ToUint8Array, uint8ArrayToBase64 } from "../utils/bytes.js";
 
 const logHead = "[CAPACITORBLE]";
 const plugin = Capacitor?.Plugins?.BetaflightBle;
-
-const base64ToUint8Array = (b64) => {
-    if (!b64) return new Uint8Array(0);
-    const binary = atob(b64);
-    const len = binary.length;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-        bytes[i] = binary.charCodeAt(i);
-    }
-    return bytes;
-};
-
-const uint8ArrayToBase64 = (bytes) => {
-    let binary = "";
-    for (let i = 0; i < bytes.byteLength; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return btoa(binary);
-};
 
 class CapacitorBle extends EventTarget {
     constructor() {
