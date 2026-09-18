dcbcfe5f52b524d76ff6dc300cc3eae284e194cb
blckmn <blckmn@users.noreply.github.com>
2026-09-19 05:48:46 +1000

    refactor(utils): convert the byte conversion helpers to TypeScript (#5532)

--- src/js/protocols/CapacitorSerial.js
+++ src/js/protocols/CapacitorSerial.js
@@ -1,5 +1,5 @@
 import { Capacitor } from "@capacitor/core";
-import { hexStringToUint8Array, uint8ArrayToHexString } from "../utils/bytes.js";
+import { hexStringToUint8Array, uint8ArrayToHexString } from "../utils/bytes";
 
 const logHead = "[CAPACITORSERIAL]";
 const BetaflightSerial = Capacitor?.Plugins?.BetaflightSerial;
